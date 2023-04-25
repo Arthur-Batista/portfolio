c5f60c13b6e631eb07e51c6eee5f04c66c4acbbd
ArthurBatista <arthurbat13.as@gmail.com>
2023-04-25 09:39:16 -0300

    adicionar botão para levar até o topo da página

--- app/app.js
+++ app/app.js
@@ -8,3 +8,20 @@ function showMenu(){
         btnMobile.src='assets/menu.svg'
     }
 }
+
+// Exibe o botão quando o usuário rolar a página para baixo
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// Faz a rolagem suave para o topo da página
+function topFunction() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+}
